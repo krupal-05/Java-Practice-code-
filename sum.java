2826b9c6bb2ae9e39836be45e6b6c6ac29c971ab
krupal-05 <krupalchaudhari500@gmail.com>
2025-02-15 00:30:54 +0530

    Second  java code(sum) add

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+ class Sum{
+	public static void main(String args[]) {
+	
+		Scanner sc = new Scanner(System.in);
+		  
+		int nu1 = sc.nextInt();
+		int nu2 = sc.nextInt();
+
+		System.out.println("First Number " + nu1);
+		System.out.println("second number " + nu2);
+		System.out.println("Sum ="+ (nu1+nu2));	
+	}
+
+}
